c114c6ce59b4f85f5328b9c04e1aad24e75ccae3
nieopierzony <43653566+nieopierzony@users.noreply.github.com>
2021-04-17 02:03:03 +0300

    feat(Navigation Category/Forum/LinkForum): добавлены структуры навигации

---
+++ src/structures/NavigationForum.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Forum = require('./Forum');
+
+class NavigationForum extends Forum {
+  constructor(client, data = {}) {
+    super(client, data);
+    this.navigationID = data.navigation_id;
+  }
+
+  _patch(data) {
+    if ('navigation_type' in data) {
+      this.navigationType = data.navigation_type;
+    } else if (typeof this.navigationType !== 'string') {
+      this.navigationType = null;
+    }
+
+    if ('navigation_depth' in data) {
+      this.navigationDepth = data.navigation_depth;
+    } else if (typeof this.navigationDepth !== 'number') {
+      this.navigationDepth = null;
+    }
+
+    if ('navigation_parent_id' in data) {
+      this.navigationParentID = data.navigation_parent_id;
+    } else if (typeof this.navigationParentID !== 'number') {
+      this.navigationParentID = null;
+    }
+
+    if ('has_sub_elements' in data) {
+      this.hasSubElements = Boolean(data.has_sub_elements);
+    }
+  }
+}
+
+module.exports = NavigationForum;
