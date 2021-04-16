b4603de76e4d4c4c1ee2364d54c683425027c04a
nieopierzony <43653566+nieopierzony@users.noreply.github.com>
2021-04-17 01:59:16 +0300

    chore(UserFieldManager/UserGroupManager): хранить класс пользователя, а не ID

--- src/managers/UserFieldManager.js
+++ src/managers/UserFieldManager.js
@@ -4,12 +4,12 @@ const BaseManager = require('./BaseManager');
 const UserField = require('../structures/UserField');
 
 class UserFieldManager extends BaseManager {
-  constructor(client, iterable, userID) {
+  constructor(client, iterable, user) {
     super(client, iterable, UserField);
-    this.userID = userID;
+    this.user = user;
   }
 
-  async fetch(id = this.userID, cache = true) {
+  async fetch(id = this.user.id, cache = true) {
     const endpoint = this.client.api.endpoints.Users.getFields(id);
     const data = await this.client.api.request('GET', endpoint);
     data.fields.forEach(field => this.add(field, cache));
--- src/managers/UserGroupManager.js
+++ src/managers/UserGroupManager.js
@@ -4,12 +4,12 @@ const BaseManager = require('./BaseManager');
 const UserGroup = require('../structures/UserGroup');
 
 class UserGroupManager extends BaseManager {
-  constructor(client, iterable, userID) {
+  constructor(client, iterable, user) {
     super(client, iterable, UserGroup);
-    this.userID = userID;
+    this.user = user;
   }
 
-  async fetch(id = this.userID, cache = true) {
+  async fetch(id = this.user.id, cache = true) {
     const endpoint = this.client.api.endpoints.Users.getGroups(id);
     const data = await this.client.api.request('GET', endpoint);
     data.user_groups.forEach(group => this.add(group, cache));
