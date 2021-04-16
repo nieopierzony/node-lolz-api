c06817c866f6b5d89c0f3375b17a531877d76fc6
nieopierzony <43653566+nieopierzony@users.noreply.github.com>
2021-04-17 01:55:35 +0300

    chore(example): обновление API ключа

--- examples/index.js
+++ examples/index.js
@@ -5,10 +5,10 @@ const { Client } = require('../src');
 
 const client = new Client({ apiURL: 'http://test1.ru/api/index.php?' });
 client
-  .login('e4c38061f180dd561ef7133cbbf71936bb71addc')
+  .login('77c9fce7693575a0b2accd9d4dca2fa6cca00be7')
   .then(async () => {
     console.log(`[Client] Авторизован как ${client.user.username} (${client.user.links.permalink})`);
     const res = await client.user.getProfilePosts();
-    console.log(res.posts[0].links);
+    console.log(await res.posts[0].edit('test134423'));
   })
   .catch(err => console.error(err, err.response ? err.response.data : null));
